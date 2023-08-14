9bfccd5d8bda0c03369baabd196d74d7d1ff9761
joycej3 <joycej3@tcd.ie>
2023-08-14 16:25:49 +0100

    Added backend

---
+++ backend_chips/src/main/java/com/joycej3/backend_chips/BackendChipsApplication.java
@@ -0,0 +1,13 @@
+package com.joycej3.backend_chips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendChipsApplication.class, args);
+    }
+
+}
---
+++ backend_chips/src/test/java/com/joycej3/backend_chips/BackendChipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joycej3.backend_chips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
